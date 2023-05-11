97f045d025b6cc2afb124c1fb843868594390e26
Genni <Genni@10.157.10.255>
2023-05-11 08:07:16 -0500

    prueba1

---
+++ src/main/java/com/mycompany/tallerf/App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tallerf;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Genni
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Integer mat1 [][] = {{1,1,1,5,5},{2,1,5,5,4},{2,1,5,4,4},{2,2,4,4,3},{2,3,3,3,3}};
+        System.out.println(Arrays.toString(mat1));
+    }
+}
